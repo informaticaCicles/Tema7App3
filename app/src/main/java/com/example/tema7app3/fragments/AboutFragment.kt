0af2a3a54717ed14d0214a8d4b9643fd6d2a82d9
Anna <informaticaCicles@gmail.com>
2023-11-21 22:33:01 +0100

    1st commit

---
+++ app/src/main/java/com/example/tema7app3/fragments/AboutFragment.kt
@@ -0,0 +1,23 @@
+package com.example.tema7app3.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tema7app3.R
+
+
+class AboutFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+
+}
